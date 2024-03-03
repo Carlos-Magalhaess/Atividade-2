3db7bfe11e7d4687d776daeb571e06bb06b4b8d5
Carlos Henrique <160761282+Carlos-Magalhaess@users.noreply.github.com>
2024-03-03 16:55:39 -0300

    Add files via upload

---
+++ Number1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Number1 { 
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Digite um numero: ");
+        int numero = sc.nextInt();
+        
+        System.out.println("O numero informado foi " + numero);
+    }
+    
+    
+}
+    
